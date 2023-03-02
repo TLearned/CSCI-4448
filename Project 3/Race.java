f8b0d83874788ead4e3ed6aaf32c0b9ded0b82ca
Tyler Learned <58530949+TLearned@users.noreply.github.com>
2023-03-01 20:38:47 -0700

    Add files via upload

---
+++ Project 3/Race.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Race {
+    //20 vehiicles(0-3 from FNCD)
+    //1 Driver per vehicle fromFNCD for cars from it
+    //Race positions
+        //1-3 win
+            //raceswon + 1
+        //15-20 car becomess damaged
+
+    racePlacing(){
+
+    }
+
+    raceAnnouncemtn({
+
+    })
+    
+}
